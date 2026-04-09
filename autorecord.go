04c15ebf4e6f9120f64b6cb4b4291438b86fd1dc
NewtonZhan <zhanyunze0601@gmail.com>
2026-04-09 09:16:33 +0800

    feat(main): Add welcome_text

--- autorecord.go
+++ autorecord.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/1Vewton/vocabulary_autorecord/data_management/config"
+	"github.com/1Vewton/vocabulary_autorecord/utils/welcome_text"
 )
 
 // Initialize: Initializes the env file
@@ -14,9 +15,11 @@ func init() {
 	} else {
 		fmt.Println("Successfully loaded env file")
 	}
+	config.InitializeSettings()
 }
 
 // main function
 func main() {
-	fmt.Println("Welcome to vocabulary_autorecord")
+	fmt.Println("Welcome to ")
+	welcome_text.WelcomeText()
 }
--- data_management/config/config.go
+++ data_management/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"github.com/1Vewton/vocabulary_autorecord/utils/env_reader"
 	"github.com/joho/godotenv"
 )
 
@@ -13,8 +14,18 @@ type env_config struct {
 	pS  float64
 }
 
+var Settings env_config
+
 // initialize configuration
 func InitializeConfig(env_path string) (err error) {
 	Err := godotenv.Load(env_path)
 	return Err
+}
+
+// Initialize settings
+func InitializeSettings() {
+	Settings.pL0 = env_reader.GetEnvFloat64("PL0", 0.000001)
+	Settings.pT = env_reader.GetEnvFloat64("PT", 0.3)
+	Settings.pG = env_reader.GetEnvFloat64("PG", 0.2)
+	Settings.pS = env_reader.GetEnvFloat64("PS", 0.05)
 }
